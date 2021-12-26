da06616013e332bef9ce2fe0ec4120a381b4b19d
JannThomas <jannschafranek@icloud.com>
2021-12-26 16:21:01 +0100

    Fix date retrieval

--- Sources/SwiftyOpenGraph/Internal/_getDate.swift
+++ Sources/SwiftyOpenGraph/Internal/_getDate.swift
@@ -27,6 +27,23 @@ import Foundation
 
 func _getDate(from string: String?) -> Date? {
     string.flatMap { (dateString: String) in
-        ISO8601DateFormatter().date(from: dateString)
+        let optionSets: [ISO8601DateFormatter.Options] = [
+            [.withFullDate, .withDashSeparatorInDate],
+            [.withInternetDateTime],
+            [.withFullDate, .withFullTime, .withSpaceBetweenDateAndTime],
+            [.withYear, .withWeekOfYear, .withDashSeparatorInDate],
+            [.withYear, .withWeekOfYear, .withDay, .withDashSeparatorInDate],
+            [.withYear, .withDay, .withDashSeparatorInDate]
+        ]
+        
+        for options in optionSets {
+            let formatter = ISO8601DateFormatter()
+            formatter.formatOptions = options
+            if let date = formatter.date(from: dateString) {
+                return date
+            }
+        }
+    
+        return nil
     }
 }
